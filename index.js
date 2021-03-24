26eba88cace74d4567b930fce57e30af37740bba
Mohammad Zakaria <zakaria.eee.iiuc@gmail.com>
2021-03-24 17:35:48 +0600

    my first commit

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
+const { response } = require('express')
 const express = require('express')
+const cors = require('cors');
+
 const app = express()
+
+app.use(cors());
+
+const users = ["Asad", "Mayin", "Humayra", "Ashraful"]
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.send('thank you for calling')
+  const fruit = {
+    product:"ada",
+    price:220
+  }
+  res.send(fruit)
 }) 
-app.listen(3000, () => console.log('listening to port 3000'))
+app.get('/fruits/banana', (req,res)=>{
+  res.send({fruit:'banana', quantity:100,price:220})
+})
+app.get('/users/:id',(req,res)=>{
+  const id = req.params.id;
+  console.log(req.query.sort);
+  const name = users[id];
+  res.send({id, name})
+  console.log(req.params.id)
+})
+
+app.post('/addUser', (req, res) => {
+  console.log('post req send');
+})
+app.listen(4200, () => console.log('listening to port 4200'))
